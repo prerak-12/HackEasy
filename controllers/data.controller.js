cc534698e0ec2818d27a5e4ab27dc06fc5fce547
Prerak <prerak.varshney@pharmeasy.in>
2026-05-23 09:04:52 +0530

    WEB-6864: Added useAi check

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1,22 +1,64 @@
 import { GoogleGenAI } from "@google/genai";
 import { getJson } from "serpapi";
 
+const handleDeepLink = (category) => {
+    switch (category) {
+        case "LAB TESTS":
+            return "push.pharmeasy.clevertap://deeplink/place_pathlab_order";
+
+        case "MEDICINES":
+            return "push.pharmeasy.clevertap://deeplink/medicine_otc_search?showAlert=true";
+
+        case "HEART HEALTH":
+            return "push.pharmeasy.clevertap://deeplink/open_web_view?url=https://pharmeasy.in/diag-pwa/content/hearthealth2";
+
+        case "PHYSIO THERAPY":
+            return "push.pharmeasy.clevertap://deeplink/open_web_view?url=https://pharmeasy.in/services/physiotherapy-2?iswebview=true&shouldCloseWebView=true&src=home_tile";
+
+        case "DOCTOR CONSULT":
+            return "push.pharmeasy.clevertap://deeplink/open_web_view?login_source=doctor-consultation&url=https://pharmeasy.in/doctor-consultation/landing?hideHeader=true&webview=true&src=home_tile";
+
+        case "BRANDED SUBSTITUTE":
+            return "push.pharmeasy.clevertap://deeplink/open_web_view?url=https://pharmeasy.in/generics?webview=true&hideHeader=true";
+
+        default:
+            return "push.pharmeasy.clevertap://deeplink/medicine_otc_search?showAlert=true";
+    }
+};
+
+const categories = `
+1) LAB TESTS,
+2) MEDICINES,
+3) HEART HEALTH,
+4) PHYSIO THERAPY,
+5) DOCTOR CONSULT,
+6) BRANDED SUBSTITUTE
+`;
+
+const aiRules = `
+Strictly follow these rules:
+- Return ONLY valid JSON
+- No markdown
+- No explanation
+- Category must exactly match one of the allowed categories
+- Description should be maximum 2 lines
+
+The JSON must very strictly follow this structure:
+{
+  "categories": {
+    "category": "CATEGORY",
+    "description": "SHORT DESCRIPTION",
+    "cta_text": "Tap here to visit CATEGORY page"
+  }
+}
+`;
+
 const generateAIData = async (ai, description) => {
     const prompt = `Analyze this data and return a JSON object categorization. 
-    Category can be 1) PHARMA, 2) DIAGNOSTICS, 3) DOCTOR CONSULT, 4) PHYSIOTHERAPY and related categories
+    Category can only be classified into these ${categories}
     One one category should be returned based on the data. The description is from a youtube video and may contain information about the video content, which can be used to determine the category.
-The JSON must strictly follow this structure:
-
-{
-  "categories": 
-    {
-      "category": "CATEGORY",
-      "description": "2-line description here",
-      "cta_text": "Tap here to land CATEGORY page"
-    }
-  
-}
-Data to analyze: ${description}`;
+    ${aiRules}
+    Data to analyze: ${description}`;
 
     const response = await ai.models.generateContent({
         model: "gemini-3.5-flash",
@@ -46,37 +88,50 @@ export const generateData = async (req, res) => {
     try {
         const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
-        const { youtubeQuery } = req.body;
+        const { youtubeQuery, useAi = true } = req.body;
         if (!Array.isArray(youtubeQuery) || youtubeQuery.length === 0) {
             return res.status(400).json({ success: false, error: "youtubeQuery must be a non-empty array" });
         }
 
-        console.log("Fetching data from SerpApi...");
         const results = await Promise.all(
             youtubeQuery.map(async (videoId) => {
                 const data = await fetchYoutubeData(videoId);
-                const description = data?.description?.content || "";
+                const description = data?.description?.content || null;
 
                 let parsedAiData = null;
-                if (description && description.trim() !== "") {
-                    console.log("Generating AI Data...");
+                let deepLink = null;
+                let isAiUsed = false;
+
+                if (useAi && description && description.trim() !== "") {
                     const generatedData = await generateAIData(ai, description);
                     if (generatedData) {
                         const cleanedString = generatedData
-                            .replace(/^```json\s*/i, "") // Removes starting ```json
-                            .replace(/```\s*$/, "")      // Removes ending ```
-                            .trim();                     // Trims trailing whitespace
+                            .replace(/^```json\s*/i, "")
+                            .replace(/```\s*$/, "")    
+                            .trim();
                         try {
                             parsedAiData = JSON.parse(cleanedString);
+                            deepLink = handleDeepLink(parsedAiData.categories.category);
+                            isAiUsed = true;
                         } catch {
                             parsedAiData = null;
+                            deepLink = null;
+                            isAiUsed = false;
                         }
                     }
                 } else {
-                    console.log("No description found to analyze.");
+                    parsedAiData = {
+                        categories: {
+                            category: "MEDICINES",
+                            description: data?.title?.trim() || "No description available",
+                            cta_text: "Tap here to visit MEDICINES"
+                        }
+                    };
+                    isAiUsed = false;
+                    deepLink = handleDeepLink(parsedAiData.categories.category);
                 }
 
-                return { data, aiData: parsedAiData, deepLink: "" };
+                return { data, aiData: parsedAiData, deepLink, isAiUsed };
             })
         );
 
@@ -84,6 +139,6 @@ export const generateData = async (req, res) => {
 
     } catch (error) {
         console.error("Error details:", error);
-        return res.status(500).json({ success: false, error: error.message || "An error occurred while fetching data" });
+        return res.status(500).json({ success: false, error: error?.message || "An error occurred while fetching data" });
     }
 }
